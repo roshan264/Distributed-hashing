a4ee6280176653ffd88f3ab9954ed1ed97927c08
Roshan <roshanshinde812000@gmail.com>
2025-04-13 00:57:24 +0530

    First cut to create distributed hashing.

---
+++ client/main.go
@@ -0,0 +1,13 @@
+/*store server -> url maps.
+
+add one by one node from server in hashring.
+give few set key-value calls.
+also fetch those values.
+
+set-key value function: 
+	convert key into hash.
+	call hashring get node function.
+	send key on given node.
+get key value function:
+	same as above.
+delete key same as above.*/
