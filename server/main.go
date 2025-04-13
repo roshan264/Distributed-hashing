c9eaf0c9433a4407b5f49139a7aebf0be8084cd3
Roshan <roshanshinde812000@gmail.com>
2025-04-13 23:47:44 +0530

    setting server to store and get the key value pairs.

--- server/main.go
+++ server/main.go
@@ -7,8 +7,52 @@
 
 package main 
 
+import(
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"encoding/json"
+	"sync"
+)
 
+var store sync.Map
+
+type keyValRequest struct {
+	Key   string `json:"key"`
+	Value string `json:"value",omitempty`
+}
 
 func main(){
+	http.HandleFunc("/set", handleSet)
+	http.HandleFunc("/get", handleGet)
 
+	port := "9001"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	} 
+	address := ":" + port
+	log.Fatal(http.ListenAndServe(address, nil))
+}
+
+func handleSet(w http.ResponseWriter, r *http.Request){
+	var req keyValRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+	fmt.Printf("key:%v value:%v", req.Key, req.Value)
+	store.Store(req.Key, req.Value)
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request){
+	key := r.URL.Query().Get("key")
+	if value, ok := store.Load(key); ok {
+		fmt.Fprintf(w, "%s", value)
+	} else {
+		http.NotFound(w, r)
+	}
+
+	fmt.Printf(key)
 }
