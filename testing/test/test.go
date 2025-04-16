8d9a80bab3746a38998a78f2495784fdc6be485e
Roshan <roshanshinde812000@gmail.com>
2025-04-16 18:19:47 +0530

    Adding test cases with proper message

--- testing/test/test.go
+++ testing/test/test.go
@@ -23,40 +23,41 @@ func DecodeValue[T any](data []byte) (T, error) {
 func UnitTesting(){
 
 	var wg sync.WaitGroup
-
-	for i := 0 ; i < 10 ; i++{
+	fmt.Printf("\n********Adding Keys to HashMap*****\n")
+	for i := 0 ; i < 8 ; i++{
 		wg.Add(1)
 		go func(i int){
 			defer wg.Done()
 			name := "Roshan" + strconv.Itoa(i)
+			key := "user" + strconv.Itoa(i)
 			age := i 
 			user := User{Name: name, Age: age}
-			err := methods.SetKeyValue(name, user)
-			fmt.Printf("set: key: %v value: %+v\n", name, age)
+			err := methods.SetKeyValue(key, user)
+			fmt.Printf("Added key: %v value: %+v\n", key, user)
 			if err != nil {
-				fmt.Printf("Error while setting :key: %v: %v \n", name, err.Error())
+				fmt.Printf("Error while setting :key: %v: %v \n", key, err.Error())
 			}
 		}(i)
 		//go putKeyGoroutine (i)
 	}
 
 	wg.Wait()
-
-	for i := 0 ; i < 10 ; i++{
+	fmt.Printf("\n******************************\n\n")
+	fmt.Printf("********Fetching Keys from hashmap*****\n")
+	for i := 0 ; i < 8 ; i++{
 		wg.Add(1)
 		getKeyGoRountine := func(i int){
 			defer wg.Done()
-			name := "Roshan" + strconv.Itoa(i)
-
-			data, err := methods.GetValue(name)
+			key := "user" + strconv.Itoa(i)
+			data, err := methods.GetValue(key)
 			if err != nil {
-				fmt.Printf("%v\n", err.Error())
+				fmt.Printf("key %v not found", key)
 				return
 			}
-			// fmt.Printf("without marshal: key: %v value: %+v\n", name, string(data))
-			val, _ := DecodeValue[User](data)
+			fmt.Printf("Fetched key: %v value: %+v\n", key, string(data))
+			// val, _ := DecodeValue[User](data)
 
-			fmt.Printf("fetching(After decoding to value format) for key: %v Name: %v value: %v\n", name, val.Name, val.Age)
+			// fmt.Printf("fetching(After decoding to value format) for key: %v value: %v \n", name, val)
 			// fmt.Printf("Print: name %v", val.Name)
 		}
 		go getKeyGoRountine(i)
@@ -64,43 +65,44 @@ func UnitTesting(){
 
 	wg.Wait()
 
+	fmt.Printf("\n******************************\n\n")
+	fmt.Printf("********Deleting few Keys from hashmap*****\n")
 
 	for i := 0 ; i < 12 ; i = i + 3{
 		wg.Add(1)
 		deleteKeyGoRountine := func(i int){
 			defer wg.Done()
-			name := "Roshan" + strconv.Itoa(i)
-
-			err := methods.DeleteKey(name)
+			key := "user" + strconv.Itoa(i)
+			err := methods.DeleteKey(key)
 			if err != nil {
-				fmt.Printf("Error while deleting %v : %v \n", name, err)
+				fmt.Printf("Error while deleting %v : %v \n", key, err)
 				return
 			}
 
-			fmt.Printf("deleted key key: %v \n", name)
+			fmt.Printf("Deleted key: %v \n", key)
 			
 		}
 		go deleteKeyGoRountine(i)
 	}
 
 	wg.Wait()
+	fmt.Printf("\n******************************\n\n")
+	fmt.Printf("********Again fetching Keys from hashmap*****\n")
 
-
-	for i := 0 ; i < 12; i++{
+	for i := 0 ; i < 10; i++{
 		wg.Add(1)
 		getKeyGoRountine := func(i int){
 			defer wg.Done()
-			name := "Roshan" + strconv.Itoa(i)
-
-			data, err := methods.GetValue(name)
+			key := "user" + strconv.Itoa(i)
+			data, err := methods.GetValue(key)
 			if err != nil {
-				fmt.Printf("%v\n", err.Error())
+				fmt.Printf("key %v not found\n", key)
 				return
 			}
-			// fmt.Printf("without marshal: key: %v value: %+v\n", name, string(data))
-			val, _ := DecodeValue[User](data)
+			fmt.Printf("Fetched: key: %v value: %+v\n", key, string(data))
+			// val, _ := DecodeValue[User](data)
 
-			fmt.Printf("Fetched(After decoding to value format) key: %v value: %v\n", name, val)
+			// fmt.Printf("fetching(After decoding to value format) for key: %v value: %v \n", name, val)
 			// fmt.Printf("Print: name %v", val.Name)
 		}
 		go getKeyGoRountine(i)
@@ -108,6 +110,49 @@ func UnitTesting(){
 
 	wg.Wait()
 
+	fmt.Printf("\n******************************\n\n")
+	fmt.Printf("********Overwrite values of some keys*****\n")
+
+	for i := 0 ; i < 10 ; i = i + 2{
+		wg.Add(1)
+		overwriteKeyGoRountine := func(i int){
+			defer wg.Done()
+			name := "Roshan-overwrite" + strconv.Itoa(i)
+			key := "user" + strconv.Itoa(i)
+			age := i 
+			user := User{Name: name, Age: age}
+			err := methods.SetKeyValue(key, user)
+			fmt.Printf("Overwrite key: %v value: %+v\n", key, user)
+			if err != nil {
+				fmt.Printf("Error while setting :key: %v: %v \n", key, err.Error())
+			}
+			
+		}
+		go overwriteKeyGoRountine(i)
+	}
+	wg.Wait()
+
+	fmt.Printf("\n******************************\n\n")
+	fmt.Printf("********Fetch Overwritten values *****\n")
+
+	for i := 0 ; i < 10  ; i = i + 2{
+		wg.Add(1)
+		overwriteKeyGoRountine := func(i int){
+			defer wg.Done()
+			key := "user" + strconv.Itoa(i)
+			data, err := methods.GetValue(key)
+			if err != nil {
+				fmt.Printf("key %v not found\n", key)
+				return
+			}
+			fmt.Printf("Fetched: key: %v value: %+v\n", key, string(data))
+
+			
+		}
+		go overwriteKeyGoRountine(i)
+	}
+	wg.Wait()
+	
 	fmt.Println("Test completed successfully!")
 }
 
